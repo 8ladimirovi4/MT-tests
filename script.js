ee3685058a09f89bf95cf7ee683462f705875bfb
Vladimir_Leonov <2227331.spb@gmail.com>
2024-01-23 15:07:11 +0300

    Добавил некоторые элементы инпутов, форм и др

---
+++ script.js
@@ -0,0 +1,21 @@
+const array = [1,2,3,4,5,6,7,8,9,10]
+
+
+for (let index = 0; index < array.length; index++) {
+    //console.count('Итерация')
+}
+
+console.table({ name: "John", age: 30, city: "New York" });
+
+console.group("Группа 1");
+console.log("Сообщение в группе 1");
+console.log("Еще одно сообщение в группе 1");
+console.groupEnd();
+
+console.log("Это снова обычное сообщение вне группы");
+
+console.group("Группа 2");
+console.log("Сообщение в группе 2");
+console.group("Вложенная группа");
+console.log("Сообщение во вложенной группе");
+console.groupEnd();
