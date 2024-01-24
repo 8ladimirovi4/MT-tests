4088e3f85412856fa1e860c8f35a18a3a8b4168c
vlleo <vladimir.leonov@systeme.ru>
2024-01-24 19:00:07 +0300

    синхронизировал script.js

--- script.js
+++ script.js
@@ -1,17 +1,3 @@
-<<<<<<< HEAD
-
-const diff = 1701733411254 - 1701647009870 
-const milliseconds = diff;
-
-// Конвертация в часы
-const hours = milliseconds / (1000 * 60 * 60);
-
-// Конвертация в дни
-const days = hours / 24;
-
-console.log(`Время в часах: ${hours}`);
-console.log(`Время в днях: ${days}`);
-=======
 const array = [1,2,3,4,5,6,7,8,9,10]
 
 
@@ -33,4 +19,3 @@ console.log("Сообщение в группе 2");
 console.group("Вложенная группа");
 console.log("Сообщение во вложенной группе");
 console.groupEnd();
->>>>>>> ee3685058a09f89bf95cf7ee683462f705875bfb
