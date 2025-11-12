0e1e76217ce019b1ba0db81b95aa807791203def
Vladimir Leonov <2227331.spb@gmail.com>
2025-11-12 19:27:00 +0300

    feat: create dotenv config for server

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 import http from 'http';
 import { WebSocketServer } from 'ws';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 const NODE_ENV = process.env.NODE_ENV || 'development';
